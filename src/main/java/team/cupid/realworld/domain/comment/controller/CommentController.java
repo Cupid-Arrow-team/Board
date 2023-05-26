16ece82223a524835feeae47a89d1c87fbab4e20
xogus3492 <wkdxogus3492@gmail.com>
2023-05-26 18:20:33 +0900

    게시글 댓글 Controller 개발 최종

--- src/main/java/team/cupid/realworld/domain/comment/controller/CommentController.java
+++ src/main/java/team/cupid/realworld/domain/comment/controller/CommentController.java
@@ -1,16 +1,12 @@
 package team.cupid.realworld.domain.comment.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-import team.cupid.realworld.domain.board.dto.*;
 import team.cupid.realworld.domain.comment.dto.*;
 import team.cupid.realworld.domain.comment.service.CommentService;
-import team.cupid.realworld.global.common.CustomPageResponse;
 import team.cupid.realworld.global.security.principal.CustomUserDetails;
 
 import javax.validation.Valid;
@@ -33,7 +29,7 @@ public class CommentController {
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
     }
 
-    @GetMapping("{boardId}")
+    @GetMapping("/{boardId}")
     public ResponseEntity<List<CommentReadResponseDto>> readComment(
             @PathVariable Long boardId
     ) {
